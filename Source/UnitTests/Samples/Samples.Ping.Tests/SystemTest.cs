c1d9b630b629a4d140eb69f00c6ba37159c27403
Rickard Nilsson <rickard@rickardnilsson.net>
2014-05-10 10:24:50 +0200

    Removed reference to Consolas.Core.Tests in a sample test project

--- Source/UnitTests/Samples/Samples.Ping.Tests/SystemTest.cs
+++ Source/UnitTests/Samples/Samples.Ping.Tests/SystemTest.cs
@@ -1,16 +1,35 @@
-﻿using Consolas.Core.Tests;
+﻿using System;
+using System.IO;
+using System.Text;
 using NUnit.Framework;
 
 namespace Samples.Ping.Tests
 {
     [TestFixture]
-    public class SystemTest : ConsoleTest
+    public class SystemTest
     {
+        private StringBuilder _consoleOut;
+        private TextWriter _outWriter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _outWriter = Console.Out;
+            _consoleOut = new StringBuilder();
+            Console.SetOut(new StringWriter(_consoleOut));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.SetOut(_outWriter);
+        }
+
         [Test]
         public void Main_ValidHost_RendersResult()
         {
             Program.Main(new [] {"-Host", "google.com"});
-            StringAssert.Contains("Reply from google.com", ConsoleOut.ToString());
+            StringAssert.Contains("Reply from google.com", _consoleOut.ToString());
         }
     }
 }
